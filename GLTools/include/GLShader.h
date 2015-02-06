1cdf6d690441c7c92e2c889ef38e15f13962f619
Zoltan Dalmadi <dmz985@gmail.com>
2015-02-06 18:51:37 +0100

    Added GLShader class

---
+++ GLTools/include/GLShader.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <string>
+
+class GLShader
+{
+public:
+
+  enum shaderType
+  {
+    VERTEX_SHADER,
+    FRAGMENT_SHADER
+  };
+
+  GLShader(shaderType type);
+  ~GLShader();
+
+  void addSource(const std::string& source);
+  bool loadSource(const std::string& filePath);
+  bool compile();
+
+  bool isCompiled() const;
+  const std::string& log() const;
+  shaderType type() const;
+  const std::string& source() const;
+  GLuint ID() const;
+
+private:
+  bool m_compileSuccess;
+  shaderType m_type;
+  bool m_loadSuccess;
+  GLint m_id;
+  std::string m_infoLog;
+  std::string m_sourceCode;
+};
+
+inline bool GLShader::isCompiled() const
+{
+  return m_compileSuccess;
+}
+
+inline const std::string& GLShader::log() const
+{
+  return m_infoLog;
+}
+
+inline GLShader::shaderType GLShader::type() const
+{
+  return m_type;
+}
+
+inline const std::string& GLShader::source() const
+{
+  return m_sourceCode;
+}
+
+inline GLuint GLShader::ID() const
+{
+  return m_id;
+}
---
+++ GLTools/src/GLShader.cpp
@@ -0,0 +1,75 @@
+#include "GLShader.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+GLShader::GLShader(shaderType type)
+  : m_type(type)
+{
+  GLuint gl_type;
+
+  switch (m_type)
+  {
+  case GLShader::VERTEX_SHADER:
+    gl_type = GL_VERTEX_SHADER;
+    break;
+
+  case GLShader::FRAGMENT_SHADER:
+    gl_type = GL_FRAGMENT_SHADER;
+    break;
+  }
+
+  m_id = glCreateShader(gl_type);
+}
+
+GLShader::~GLShader()
+{
+  glDeleteShader(m_id);
+}
+
+void GLShader::addSource(const std::string& source)
+{
+  m_sourceCode = source;
+
+  const char *c_str = source.c_str();
+  glShaderSource(m_id, 1, &c_str, nullptr);
+}
+
+bool GLShader::loadSource(const std::string& filePath)
+{
+  std::stringstream buffer;
+
+  std::ifstream sourceFile;
+  sourceFile.exceptions(std::ifstream::failbit);
+
+  try
+  {
+    sourceFile.open(filePath);
+    buffer << sourceFile.rdbuf();
+    addSource(buffer.str());
+  }
+  catch (std::ifstream::failure e)
+  {
+    std::cerr << "Could not find shader file: " << filePath << std::endl;
+    return false;
+  }
+
+  sourceFile.close();
+
+  return true;
+}
+
+bool GLShader::compile()
+{
+  glCompileShader(m_id);
+
+  GLint success;
+  glGetShaderiv(m_id, GL_COMPILE_STATUS, &success);
+  m_compileSuccess = success ? true : false;
+
+  char infolog[512];
+  glGetShaderInfoLog(m_id, 512, nullptr, infolog);
+  m_infoLog = infolog;
+
+  return m_compileSuccess;
+}
