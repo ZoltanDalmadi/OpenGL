de9c2bda1e62bc73ffb4356710bb4807c6fe1e70
Zoltan Dalmadi <dmz985@gmail.com>
2015-03-05 18:22:12 +0100

    Implemented methods

--- GLTools/src/GLShaderProgram.cpp
+++ GLTools/src/GLShaderProgram.cpp
@@ -1,9 +1,35 @@
 #include "GLShaderProgram.h"
 
-
 GLShaderProgram::GLShaderProgram()
-{}
-
+  : m_id(glCreateProgram())
+{
+}
 
 GLShaderProgram::~GLShaderProgram()
-{}
+{
+}
+
+void GLShaderProgram::addShader(const GLShader& shader)
+{
+  glAttachShader(m_id, shader.ID());
+}
+
+bool GLShaderProgram::link()
+{
+  glLinkProgram(m_id);
+
+  GLint success;
+  glGetProgramiv(m_id, GL_LINK_STATUS, &success);
+  m_linkSuccess = success ? true : false;
+
+  char infolog[512];
+  glGetProgramInfoLog(m_id, 512, nullptr, infolog);
+  m_infoLog = infolog;
+
+  return m_linkSuccess;
+}
+
+void GLShaderProgram::use() const
+{
+  glUseProgram(m_id);
+}
