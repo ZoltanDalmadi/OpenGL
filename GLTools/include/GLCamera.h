1962436750a53b6286fe868ffc5dfcb0ef212799
Zoltan Dalmadi <dmz985@gmail.com>
2015-03-21 12:26:03 +0100

    Added GLCamera class

---
+++ GLTools/include/GLCamera.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <glm/glm.hpp>
+
+using glm::vec3;
+using glm::mat4;
+
+class GLCamera
+{
+public:
+  GLCamera(const vec3& position);
+  GLCamera(const vec3& position, const vec3& target);
+  GLCamera(const vec3& position, const vec3& target, const vec3& up);
+  virtual ~GLCamera();
+
+  void clearTarget();
+  void orbit(const vec3& target, float angle, float radius);
+
+  void setPosition(const vec3& position);
+  const vec3& getPosition() const;
+
+  void setTarget(const vec3& target);
+  const vec3& getTarget() const;
+
+  const vec3& getDirection() const;
+
+  const mat4& getViewMatrix() const;
+  void update();
+
+  bool hasTarget() const;
+
+  void dolly(float delta);
+  void pan(float delta);
+
+  void rotate(float pitch, float yaw);
+
+private:
+  float m_pitch;
+  float m_yaw;
+  float m_roll;
+  vec3 m_position;
+  vec3 m_target;
+  vec3 m_direction;
+  vec3 m_up;
+
+  mat4 m_viewMatrix;
+
+  bool m_hasTarget = true;
+};
+
+inline void GLCamera::setPosition(const vec3& position)
+{
+  this->m_position = position;
+}
+
+inline const vec3& GLCamera::getPosition() const
+{
+  return m_position;
+}
+
+inline void GLCamera::setTarget(const vec3& target)
+{
+  this->m_target = target;
+}
+
+inline const vec3& GLCamera::getTarget() const
+{
+  return m_target;
+}
+
+inline const vec3& GLCamera::getDirection() const
+{
+  return m_direction;
+}
+
+inline const mat4& GLCamera::getViewMatrix() const
+{
+  return m_viewMatrix;
+}
+
+inline void GLCamera::update()
+{
+  this->m_viewMatrix = glm::lookAt(m_position, m_position - m_direction, m_up);
+}
+
+inline bool GLCamera::hasTarget() const
+{
+  return m_hasTarget;
+}
---
+++ GLTools/src/GLCamera.cpp
@@ -0,0 +1,76 @@
+#include <glm/gtc/matrix_transform.hpp>
+#include "GLCamera.h"
+
+GLCamera::GLCamera(const vec3& position)
+  : GLCamera(position, vec3(0.0f), vec3(0.0f, 1.0f, 0.0f))
+{
+}
+
+GLCamera::GLCamera(const vec3& position, const vec3& target)
+  : GLCamera(position, target, vec3(0.0f, 1.0f, 0.0f))
+{
+}
+
+GLCamera::GLCamera(const vec3& position, const vec3& target, const vec3& up)
+  : m_pitch(0),
+    m_yaw(-90),
+    m_roll(0),
+    m_position(position),
+    m_target(target),
+    m_direction(glm::normalize(position - target)),
+    m_up(up),
+    m_viewMatrix(glm::lookAt(position, target, up)),
+    m_hasTarget(true)
+{
+}
+
+GLCamera::~GLCamera()
+{
+}
+
+void GLCamera::clearTarget()
+{
+  m_target = m_position - m_direction;
+  m_hasTarget = false;
+  update();
+}
+
+void GLCamera::orbit(const vec3& target, float angle, float radius)
+{
+  m_target = target;
+  m_hasTarget = true;
+  float cameraX = m_target.x + std::sin(angle) * radius;
+  float cameraZ = m_target.z + std::cos(angle) * radius;
+  m_position = vec3(cameraX, target.y, cameraZ);
+  m_direction = glm::normalize(m_position - m_target);
+  m_up = vec3(0.0f, 1.0f, 0.0f);
+  this->m_viewMatrix = glm::lookAt(m_position, m_target, m_up);
+}
+
+void GLCamera::dolly(float delta)
+{
+  m_position += m_direction * delta;
+  update();
+}
+
+void GLCamera::pan(float delta)
+{
+  vec3 right = glm::normalize(glm::cross(m_direction, m_up));
+  m_position += right * delta;
+  m_target += right * delta;
+  update();
+}
+
+void GLCamera::rotate(float pitch, float yaw)
+{
+  m_pitch += pitch;
+  m_yaw += yaw;
+  glm::vec3 front;
+  front.x = std::cos(glm::radians(m_pitch)) * std::cos(glm::radians(m_yaw));
+  front.y = std::sin(glm::radians(m_pitch));
+  front.z = std::cos(glm::radians(m_pitch)) * std::sin(glm::radians(m_yaw));
+  m_direction = glm::normalize(front);
+  vec3 right = glm::normalize(glm::cross(m_direction, vec3(0.0f, 1.0f, 0.0f)));
+  m_up = glm::normalize(glm::cross(right, m_direction));
+  update();
+}
