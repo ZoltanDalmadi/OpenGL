0180c8c1532b67bfe01baabc0eafeca0ed31e75b
Zoltan Dalmadi <dmz985@gmail.com>
2015-03-28 11:46:50 +0100

    Simplify, cleanup

--- GLTools/include/GLVertexArrayObject.h
+++ GLTools/include/GLVertexArrayObject.h
@@ -14,12 +14,8 @@ public:
   void unbind();
   GLuint ID() const;
 
-  static void setVertexAttrib(GLuint index, GLint size, GLenum type,
-                              GLboolean normalize, GLint stride, GLint offset);
-
 private:
   GLuint m_id;
-  static size_t getGLTypeSize(GLenum type);
 };
 
 inline GLuint GLVertexArrayObject::ID() const
--- GLTools/src/GLVertexArrayObject.cpp
+++ GLTools/src/GLVertexArrayObject.cpp
@@ -24,32 +24,3 @@ void GLVertexArrayObject::unbind()
 {
   glBindVertexArray(0);
 }
-
-void GLVertexArrayObject::setVertexAttrib(
-  GLuint index, GLint size, GLenum type, GLboolean normalize, GLint stride,
-  GLint offset)
-{
-  size_t typeSize = getGLTypeSize(type);
-  glVertexAttribPointer(index, size, type, normalize,
-                        static_cast<GLsizei>(stride * typeSize),
-                        reinterpret_cast<GLvoid *>(offset * typeSize));
-  glEnableVertexAttribArray(index);
-}
-
-size_t GLVertexArrayObject::getGLTypeSize(GLenum type)
-{
-  size_t size;
-
-  switch (type)
-  {
-  case GL_FLOAT:
-    size = sizeof(GLfloat);
-    break;
-
-  case GL_DOUBLE:
-    size = sizeof(GLdouble);
-    break;
-  }
-
-  return size;
-}
