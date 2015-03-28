199d78cd7c77ccbe507c0154e932f11b22c33a08
Zoltan Dalmadi <dmz985@gmail.com>
2015-03-28 11:48:18 +0100

    Added useful methods for manipulating GLSL uniforms, added shader container member

--- GLTools/include/GLShaderProgram.h
+++ GLTools/include/GLShaderProgram.h
@@ -1,5 +1,11 @@
 #pragma once
+#include <vector>
+#include <memory>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "GLShader.h"
+
+using GLShader_ptr = std::shared_ptr<GLShader>;
 
 class GLShaderProgram
 {
@@ -7,18 +13,50 @@ public:
   GLShaderProgram();
   virtual ~GLShaderProgram();
 
-  void addShader(const GLShader& shader);
+  void addShader(const GLShader_ptr& shader);
   bool link();
+  bool create();
+  void destroy();
   void use() const;
+
+  void setAttributeArray
+  (GLint location, GLint size, size_t offset, GLsizei stride = 0);
+
+  GLint uniformLocation(const char *name) const;
+  GLint uniformLocation(const std::string& name) const;
+
+  void setUniformValue(GLint location, float x, float y);
+  void setUniformValue(const char *name, float x, float y);
+  void setUniformValue(GLint location, const glm::vec2& value);
+  void setUniformValue(const char *name, const glm::vec2& value);
+  void setUniformValue(GLint location, float x, float y, float z);
+  void setUniformValue(const char *name, float x, float y, float z);
+  void setUniformValue(GLint location, const glm::vec3& value);
+  void setUniformValue(const char *name, const glm::vec3& value);
+  void setUniformValue(GLint location, float x, float y, float z, float w);
+  void setUniformValue(const char *name, float x, float y, float z, float w);
+  void setUniformValue(GLint location, const glm::vec4& value);
+  void setUniformValue(const char *name, const glm::vec4& value);
+
+  void setUniformValue
+  (GLint location, const glm::mat3& value, GLboolean transpose = GL_FALSE);
+  void setUniformValue
+  (const char *name, const glm::mat3& value, GLboolean transpose = GL_FALSE);
+  void setUniformValue
+  (GLint location, const glm::mat4& value, GLboolean transpose = GL_FALSE);
+  void setUniformValue
+  (const char *name, const glm::mat4& value, GLboolean transpose = GL_FALSE);
 
   bool isLinked() const;
   const std::string& log() const;
   GLuint ID() const;
+  std::vector<GLShader_ptr> shaders() const;
 
 private:
   GLuint m_id;
   bool m_linkSuccess = false;
   std::string m_infoLog;
+  std::vector<GLShader_ptr> m_shaders;
 };
 
 inline bool GLShaderProgram::isLinked() const
@@ -34,4 +72,9 @@ inline const std::string& GLShaderProgram::log() const
 inline GLuint GLShaderProgram::ID() const
 {
   return m_id;
+}
+
+inline std::vector<GLShader_ptr> GLShaderProgram::shaders() const
+{
+  return m_shaders;
 }
--- GLTools/src/GLShaderProgram.cpp
+++ GLTools/src/GLShaderProgram.cpp
@@ -1,17 +1,30 @@
 #include "GLShaderProgram.h"
 
 GLShaderProgram::GLShaderProgram()
-  : m_id(glCreateProgram())
 {
 }
 
 GLShaderProgram::~GLShaderProgram()
 {
+  destroy();
 }
 
-void GLShaderProgram::addShader(const GLShader& shader)
+bool GLShaderProgram::create()
 {
-  glAttachShader(m_id, shader.ID());
+  this->m_id = glCreateProgram();
+  return m_id ? true : false;
+}
+
+void GLShaderProgram::destroy()
+{
+  glDeleteProgram(m_id);
+}
+
+void GLShaderProgram::addShader(const std::shared_ptr<GLShader>& shader)
+{
+  glAttachShader(m_id, shader->ID());
+  this->m_shaders.push_back(shader);
+  this->m_linkSuccess = false;
 }
 
 bool GLShaderProgram::link()
@@ -22,9 +35,12 @@ bool GLShaderProgram::link()
   glGetProgramiv(m_id, GL_LINK_STATUS, &success);
   m_linkSuccess = success ? true : false;
 
-  char infolog[512];
-  glGetProgramInfoLog(m_id, 512, nullptr, infolog);
-  m_infoLog = infolog;
+  if (!m_linkSuccess)
+  {
+    char infolog[512];
+    glGetProgramInfoLog(m_id, 512, nullptr, infolog);
+    m_infoLog = infolog;
+  }
 
   return m_linkSuccess;
 }
@@ -32,4 +48,110 @@ bool GLShaderProgram::link()
 void GLShaderProgram::use() const
 {
   glUseProgram(m_id);
+}
+
+void GLShaderProgram::setAttributeArray
+(GLint location, GLint size, size_t offset, GLsizei stride)
+{
+  glVertexAttribPointer
+  (location, size, GL_FLOAT, GL_FALSE, stride,
+   reinterpret_cast<GLvoid *>(offset));
+  glEnableVertexAttribArray(location);
+}
+
+GLint GLShaderProgram::uniformLocation(const char *name) const
+{
+  return glGetUniformLocation(m_id, name);
+}
+
+GLint GLShaderProgram::uniformLocation(const std::string& name) const
+{
+  return uniformLocation(name.c_str());
+}
+
+void GLShaderProgram::setUniformValue(GLint location, float x, float y)
+{
+  glUniform2f(location, x, y);
+}
+
+void GLShaderProgram::setUniformValue(const char *name, float x, float y)
+{
+  setUniformValue(uniformLocation(name), x, y);
+}
+
+void GLShaderProgram::setUniformValue(GLint location, const glm::vec2& value)
+{
+  setUniformValue(location, value.x, value.y);
+}
+
+void GLShaderProgram::setUniformValue(const char *name, const glm::vec2& value)
+{
+  setUniformValue(uniformLocation(name), value.x, value.y);
+}
+
+void GLShaderProgram::setUniformValue(GLint location, float x, float y, float z)
+{
+  glUniform3f(location, x, y, z);
+}
+
+void GLShaderProgram::setUniformValue(const char *name, float x, float y,
+                                      float z)
+{
+  setUniformValue(uniformLocation(name), x, y, z);
+}
+
+void GLShaderProgram::setUniformValue(GLint location, const glm::vec3& value)
+{
+  setUniformValue(location, value.x, value.y, value.z);
+}
+
+void GLShaderProgram::setUniformValue(const char *name, const glm::vec3& value)
+{
+  setUniformValue(uniformLocation(name), value.x, value.y, value.z);
+}
+
+void GLShaderProgram::setUniformValue(GLint location, float x, float y, float z,
+                                      float w)
+{
+  glUniform4f(location, x, y, z, w);
+}
+
+void GLShaderProgram::setUniformValue(const char *name, float x, float y,
+                                      float z, float w)
+{
+  setUniformValue(uniformLocation(name), x, y, z, w);
+}
+
+void GLShaderProgram::setUniformValue(GLint location, const glm::vec4& value)
+{
+  setUniformValue(location, value.x, value.y, value.z, value.w);
+}
+
+void GLShaderProgram::setUniformValue(const char *name, const glm::vec4& value)
+{
+  setUniformValue(uniformLocation(name), value.x, value.y, value.z, value.w);
+}
+
+void GLShaderProgram::setUniformValue
+(GLint location, const glm::mat3& value, GLboolean transpose)
+{
+  glUniformMatrix3fv(location, 1, transpose, glm::value_ptr(value));
+}
+
+void GLShaderProgram::setUniformValue
+(const char *name, const glm::mat3& value, GLboolean transpose)
+{
+  setUniformValue(uniformLocation(name), value, transpose);
+}
+
+void GLShaderProgram::setUniformValue
+(GLint location, const glm::mat4& value, GLboolean transpose)
+{
+  glUniformMatrix4fv(location, 1, transpose, glm::value_ptr(value));
+}
+
+void GLShaderProgram::setUniformValue
+(const char *name, const glm::mat4& value, GLboolean transpose)
+{
+  setUniformValue(uniformLocation(name), value, transpose);
 }
