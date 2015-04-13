e5523dd4726d7e5fd5df0c7883a43a206bdfa8cd
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-13 18:01:01 +0200

    Optimization, ball rotation, cube move constraints, smart pointers, usage of GLTexture class

--- Homework2/src/main.cpp
+++ Homework2/src/main.cpp
@@ -21,26 +21,17 @@
 #include "GLSpotLight.h"
 #include "GLSphere.h"
 #include "GLCube.h"
+#include "GLTexture.h"
 
+// CONSTANTS ------------------------------------------------------------------
 const GLuint WIDTH = 1280;
 const GLuint HEIGHT = 720;
 
 const float ROOMSIZE = 24.0f;
 const float CUBESIZE = 5.0f;
 const float BALLRADIUS = 1.0f;
-
-GLFWwindow *window;
-
-glm::mat3 normalMatrix;
-glm::mat4 projection;
-
-GLTools::GLTargetCamera camera(glm::vec3(10.0f, 10.0f, 10.0f));
-
-std::vector<GLTools::GLSpotLight> spotLights;
-
-GLTools::GLPointLight pointLight;
-
-glm::vec3 cubePos;
+const float BALLSPEED = 0.4f;
+const glm::vec3 CAMERAPOS = glm::vec3(10.0f, 10.0f, 10.0f);
 
 struct CollisionPlane
 {
@@ -48,12 +39,21 @@ struct CollisionPlane
   glm::vec3 normal;
 };
 
-std::vector<CollisionPlane> walls;
-std::vector<CollisionPlane> cubeFaces;
+GLFWwindow *window;
 
+GLTools::GLTargetCamera camera(CAMERAPOS);
+
+std::vector<GLTools::GLSpotLight> spotLights;
+GLTools::GLPointLight pointLight;
+
+glm::vec3 cubePos;
 glm::vec3 ballPos(5.0f, -5.0f, -5.0f);
 glm::vec3 ballDirection;
-float ballSpeed = 0.2f;
+glm::vec3 ballRotationAxis;
+float ballRotationAngle = 0.0f;
+
+std::vector<CollisionPlane> walls;
+std::vector<CollisionPlane> cubeFaces;
 
 bool keys[1024];
 
@@ -71,30 +71,70 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action,
 
 void moveCube()
 {
+  bool moved = false;
   glm::vec3 delta(0.0f);
 
   if (keys[GLFW_KEY_W])
-    delta -= glm::vec3(0.0f, 0.0f, 0.1f);
+  {
+    if (cubePos.z - (CUBESIZE / 2.0f) >= -ROOMSIZE / 2.0f)
+    {
+      delta -= glm::vec3(0.0f, 0.0f, 0.1f);
+      moved = true;
+    }
+  }
 
   if (keys[GLFW_KEY_S])
-    delta += glm::vec3(0.0f, 0.0f, 0.1f);
+  {
+    if (cubePos.z + (CUBESIZE / 2.0f) <= ROOMSIZE / 2.0f)
+    {
+      delta += glm::vec3(0.0f, 0.0f, 0.1f);
+      moved = true;
+    }
+  }
 
   if (keys[GLFW_KEY_A])
-    delta -= glm::vec3(0.1f, 0.0f, 0.0f);
+  {
+    if (cubePos.x - (CUBESIZE / 2.0f) >= -ROOMSIZE / 2.0f)
+    {
+      delta -= glm::vec3(0.1f, 0.0f, 0.0f);
+      moved = true;
+    }
+  }
 
   if (keys[GLFW_KEY_D])
-    delta += glm::vec3(0.1f, 0.0f, 0.0f);
+  {
+    if (cubePos.x + (CUBESIZE / 2.0f) <= ROOMSIZE / 2.0f)
+    {
+      delta += glm::vec3(0.1f, 0.0f, 0.0f);
+      moved = true;
+    }
+  }
 
   if (keys[GLFW_KEY_Q])
-    delta += glm::vec3(0.0f, 0.1f, 0.0f);
+  {
+    if (cubePos.y + (CUBESIZE / 2.0f) <= ROOMSIZE / 2.0f)
+    {
+      delta += glm::vec3(0.0f, 0.1f, 0.0f);
+      moved = true;
+    }
+  }
 
   if (keys[GLFW_KEY_E])
-    delta -= glm::vec3(0.0f, 0.1f, 0.0f);
+  {
+    if (cubePos.y - (CUBESIZE / 2.0f) >= -ROOMSIZE / 2.0f)
+    {
+      delta -= glm::vec3(0.0f, 0.1f, 0.0f);
+      moved = true;
+    }
+  }
 
-  cubePos += delta;
+  if (moved)
+  {
+    cubePos += delta;
 
-  for (auto& cubeFace : cubeFaces)
-    cubeFace.origin += delta;
+    for (auto& cubeFace : cubeFaces)
+      cubeFace.origin += delta;
+  }
 }
 
 // SHADERS --------------------------------------------------------------------
@@ -102,49 +142,24 @@ void setupShaders(GLTools::GLShaderProgram& shaderProgram)
 {
   auto vertexShader =
     std::make_shared<GLTools::GLShader>
-    (GLTools::GLShader::shaderType::VERTEX_SHADER);
-  vertexShader->loadSource("shaders/vertex_shader.glsl");
-  vertexShader->compile();
-  std::cout << vertexShader->log() << std::endl;
+    (GLTools::GLShader::shaderType::VERTEX_SHADER,
+     "shaders/vertex_shader.glsl");
 
   auto fragmentShader =
     std::make_shared<GLTools::GLShader>
-    (GLTools::GLShader::shaderType::FRAGMENT_SHADER);
-  fragmentShader->loadSource("shaders/fragment_shader.glsl");
-  fragmentShader->compile();
-  std::cout << fragmentShader->log() << std::endl;
+    (GLTools::GLShader::shaderType::FRAGMENT_SHADER,
+     "shaders/fragment_shader.glsl");
 
   shaderProgram.create();
   shaderProgram.addShader(vertexShader);
   shaderProgram.addShader(fragmentShader);
   shaderProgram.link();
-  std::cout << shaderProgram.log() << std::endl;
 }
 
-GLuint loadTexture(const char *path)
+void setLightTargets(std::vector<GLTools::GLSpotLight>& lights,
+                     const glm::vec3& target)
 {
-  GLuint textureID;
-  glGenTextures(1, &textureID);
-  int width, height;
-  unsigned char *image = SOIL_load_image(path, &width, &height, 0, SOIL_LOAD_RGB);
-
-  glBindTexture(GL_TEXTURE_2D, textureID);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-               GL_UNSIGNED_BYTE, image);
-  glGenerateMipmap(GL_TEXTURE_2D);
-
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glBindTexture(GL_TEXTURE_2D, 0);
-  SOIL_free_image_data(image);
-  return textureID;
-}
-
-void setLightTargets(const glm::vec3& target)
-{
-  for (auto& light : spotLights)
+  for (auto& light : lights)
     light.setTarget(target);
 }
 
@@ -163,7 +178,10 @@ void wallCollision()
     distance = glm::dot(ballPos - wall.origin, wall.normal);
 
     if (distance < BALLRADIUS)
+    {
       ballDirection = glm::normalize(glm::reflect(ballDirection, wall.normal));
+      ballRotationAxis = glm::cross(ballDirection, wall.normal);
+    }
   }
 }
 
@@ -191,6 +209,7 @@ void cubeCollision()
   {
     ballDirection =
       glm::normalize(glm::reflect(ballDirection, cubeFaces[0].normal));
+    ballRotationAxis = glm::cross(ballDirection, cubeFaces[0].normal);
   }
 
   if (ballIntersectsPlane(ballPos, cubeFaces[1]) &&
@@ -198,6 +217,7 @@ void cubeCollision()
   {
     ballDirection =
       glm::normalize(glm::reflect(ballDirection, cubeFaces[1].normal));
+    ballRotationAxis = glm::cross(ballDirection, cubeFaces[1].normal);
   }
 
   if (ballIntersectsPlane(ballPos, cubeFaces[2]) &&
@@ -205,6 +225,7 @@ void cubeCollision()
   {
     ballDirection =
       glm::normalize(glm::reflect(ballDirection, cubeFaces[2].normal));
+    ballRotationAxis = glm::cross(ballDirection, cubeFaces[2].normal);
   }
 
   if (ballIntersectsPlane(ballPos, cubeFaces[3]) &&
@@ -212,6 +233,7 @@ void cubeCollision()
   {
     ballDirection =
       glm::normalize(glm::reflect(ballDirection, cubeFaces[3].normal));
+    ballRotationAxis = glm::cross(ballDirection, cubeFaces[3].normal);
   }
 
   if (ballIntersectsPlane(ballPos, cubeFaces[4]) &&
@@ -219,6 +241,7 @@ void cubeCollision()
   {
     ballDirection =
       glm::normalize(glm::reflect(ballDirection, cubeFaces[4].normal));
+    ballRotationAxis = glm::cross(ballDirection, cubeFaces[4].normal);
   }
 
   if (ballIntersectsPlane(ballPos, cubeFaces[5]) &&
@@ -226,6 +249,7 @@ void cubeCollision()
   {
     ballDirection =
       glm::normalize(glm::reflect(ballDirection, cubeFaces[5].normal));
+    ballRotationAxis = glm::cross(ballDirection, cubeFaces[5].normal);
   }
 }
 
@@ -268,13 +292,12 @@ void init()
   spotLights.back().setShaderName("spotLight[3]");
   spotLights.back().m_energy.second = 10.0f;
 
-  projection = glm::perspective(45.0f, WIDTH / (HEIGHT * 1.0f), 0.1f, 50.0f);
-
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<float> dis(-2, 2);
 
   ballDirection = glm::normalize(glm::vec3(dis(gen), dis(gen), dis(gen)));
+  ballRotationAxis = glm::cross(ballDirection, glm::vec3(0.0f, 1.0f, 0.0f));
 
   glPointSize(20.0f);
 }
@@ -283,42 +306,51 @@ int main()
 {
   init();
 
-  GLTools::GLShaderProgram shaderProgram;
-  setupShaders(shaderProgram);
+  auto shaderProgram = std::make_unique<GLTools::GLShaderProgram>();
+  setupShaders(*shaderProgram);
 
-  GLTools::GLSphere ball(BALLRADIUS, 24, 24);
-  ball.initialize();
+  auto ball = std::make_unique<GLTools::GLSphere>(BALLRADIUS, 24, 24);
+  ball->initialize();
 
-  GLTools::GLCube room(ROOMSIZE, true);
-  room.initialize();
+  auto room = std::make_unique<GLTools::GLCube>(ROOMSIZE, true);
+  room->initialize();
 
-  GLTools::GLCube cube(CUBESIZE);
-  cube.initialize();
+  auto cube = std::make_unique<GLTools::GLCube>(CUBESIZE);
+  cube->initialize();
 
   CollisionPlane cp;
 
   for (size_t i = 0; i < 24; i += 4)
   {
-    cp.origin = room.m_vertices[i].position;
-    cp.normal = room.m_vertices[i].normal;
+    cp.origin = room->m_vertices[i].position;
+    cp.normal = room->m_vertices[i].normal;
     walls.push_back(cp);
   }
 
   for (size_t i = 0; i < 24; i += 4)
   {
-    cp.origin = cube.m_vertices[i].position;
-    cp.normal = cube.m_vertices[i].normal;
+    cp.origin = cube->m_vertices[i].position;
+    cp.normal = cube->m_vertices[i].normal;
     cubeFaces.push_back(cp);
   }
 
-  GLuint ballTexture = loadTexture("textures/ball.png");
-  GLuint ballSpecularTexture = loadTexture("textures/ball_specular.png");
+  auto ballTexture =
+    std::make_unique<GLTools::GLTexture>("textures/ball.png");
+  auto ballSpecularTexture =
+    std::make_unique<GLTools::GLTexture>("textures/ball_specular.png");
 
-  GLuint wallTexture = loadTexture("textures/brick.jpg");
-  GLuint wallSpecularTexture = loadTexture("textures/brick_specular.jpg");
+  auto wallTexture =
+    std::make_unique<GLTools::GLTexture>("textures/brick.jpg");
+  auto wallSpecularTexture =
+    std::make_unique<GLTools::GLTexture>("textures/brick_specular.jpg");
 
-  GLuint cubeTexture = loadTexture("textures/crate.jpg");
-  GLuint cubeSpecularTexture = loadTexture("textures/crate_specular.jpg");
+  auto cubeTexture =
+    std::make_unique<GLTools::GLTexture>("textures/crate.jpg");
+  auto cubeSpecularTexture =
+    std::make_unique<GLTools::GLTexture>("textures/crate_specular.jpg");
+
+  glm::mat4 projection
+    = glm::perspective(45.0f, WIDTH / (HEIGHT * 1.0f), 0.1f, 50.0f);
 
   while (!glfwWindowShouldClose(window))
   {
@@ -326,55 +358,53 @@ int main()
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    shaderProgram.use();
-    shaderProgram.setUniformValue("view", camera.m_viewMatrix);
-    shaderProgram.setUniformValue("projection", projection);
-    shaderProgram.setUniformValue("camPos", camera.m_position);
+    shaderProgram->use();
+    shaderProgram->setUniformValue("view", camera.m_viewMatrix);
+    shaderProgram->setUniformValue("projection", projection);
+    shaderProgram->setUniformValue("camPos", camera.m_position);
 
-    shaderProgram.setUniformValue("material.diffuse", 0);
-    shaderProgram.setUniformValue("material.specular", 1);
-    shaderProgram.setUniformValue("material.shininess", 32.0f);
+    shaderProgram->setUniformValue("material.diffuse", 0);
+    shaderProgram->setUniformValue("material.specular", 1);
+    shaderProgram->setUniformValue("material.shininess", 32.0f);
 
-    pointLight.setShaderUniform(shaderProgram);
-    setLightUniforms(shaderProgram);
+    pointLight.setShaderUniform(*shaderProgram);
+    setLightUniforms(*shaderProgram);
 
     glm::mat4 roomModel;
     glm::mat3 normalMatrix = glm::mat3(glm::transpose(glm::inverse(roomModel)));
-    shaderProgram.setUniformValue("model", roomModel);
-    shaderProgram.setUniformValue("normalMatrix", normalMatrix);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, wallTexture);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, wallSpecularTexture);
-    room.draw();
+    shaderProgram->setUniformValue("model", roomModel);
+    shaderProgram->setUniformValue("normalMatrix", normalMatrix);
+    wallTexture->bind(0);
+    wallSpecularTexture->bind(1);
+    room->draw();
 
     glm::mat4 cubeModel;
     cubeModel = glm::translate(cubeModel, cubePos);
     normalMatrix = glm::mat3(glm::transpose(glm::inverse(cubeModel)));
-    shaderProgram.setUniformValue("model", cubeModel);
-    shaderProgram.setUniformValue("normalMatrix", normalMatrix);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, cubeTexture);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, cubeSpecularTexture);
-    cube.draw();
+    shaderProgram->setUniformValue("model", cubeModel);
+    shaderProgram->setUniformValue("normalMatrix", normalMatrix);
+    cubeTexture->bind(0);
+    cubeSpecularTexture->bind(1);
+    cube->draw();
 
     glm::mat4 ballModel;
     ballModel = glm::translate(ballModel, ballPos);
+    ballModel =
+      glm::rotate(ballModel, glm::radians(ballRotationAngle), ballRotationAxis);
     normalMatrix = glm::mat3(glm::transpose(glm::inverse(ballModel)));
-    shaderProgram.setUniformValue("model", ballModel);
-    shaderProgram.setUniformValue("normalMatrix", normalMatrix);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, ballTexture);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, ballSpecularTexture);
-    ball.draw();
+    shaderProgram->setUniformValue("model", ballModel);
+    shaderProgram->setUniformValue("normalMatrix", normalMatrix);
+    ballTexture->bind(0);
+    ballSpecularTexture->bind(1);
+    ball->draw();
 
     glfwSwapBuffers(window);
 
     moveCube();
-    setLightTargets(cubePos);
-    ballPos += ballDirection * ballSpeed;
+    setLightTargets(spotLights, cubePos);
+    ballPos += ballDirection * BALLSPEED;
+    ballRotationAngle > 360 ? ballRotationAngle = 0.0f : ballRotationAngle += 5.0f;
+    std::cout << ballRotationAngle << std::endl;
     camera.setTarget(ballPos);
     wallCollision();
     cubeCollision();
