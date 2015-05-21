0bf3c7fdb98e96e1c660487b230a8e6e457d2f01
Zoltan Dalmadi <dmz985@gmail.com>
2015-05-22 00:45:12 +0200

    Fixed winding and flipped normals

--- GLTools/src/GLPlane.cpp
+++ GLTools/src/GLPlane.cpp
@@ -7,26 +7,26 @@ GLTools::GLPlane::GLPlane(float width, float height)
   Vertex vert;
 
   vert.position = glm::vec3(-width2, -height2, 0);
-  vert.normal = glm::vec3(0.0f, 0.0f, 1.0f);
+  vert.normal = glm::vec3(0.0f, 0.0f, -1.0f);
   vert.texCoords = glm::vec2(1.0f, 1.0f);
   m_vertices.push_back(vert);
 
   vert.position = glm::vec3(width2, -height2, 0);
-  vert.normal = glm::vec3(0.0f, 0.0f, 1.0f);
+  vert.normal = glm::vec3(0.0f, 0.0f, -1.0f);
   vert.texCoords = glm::vec2(0.0f, 1.0f);
   m_vertices.push_back(vert);
 
   vert.position = glm::vec3(width2, height2, 0);
-  vert.normal = glm::vec3(0.0f, 0.0f, 1.0f);
+  vert.normal = glm::vec3(0.0f, 0.0f, -1.0f);
   vert.texCoords = glm::vec2(0.0f, 0.0f);
   m_vertices.push_back(vert);
 
   vert.position = glm::vec3(-width2, height2, 0);
-  vert.normal = glm::vec3(0.0f, 0.0f, 1.0f);
+  vert.normal = glm::vec3(0.0f, 0.0f, -1.0f);
   vert.texCoords = glm::vec2(1.0f, 0.0f);
   m_vertices.push_back(vert);
 
-  m_indices = { 0, 1, 2, 0, 2, 3 };
+  m_indices = { 0, 2, 1, 0, 3, 2 };
 }
 
 GLTools::GLPlane::~GLPlane()
