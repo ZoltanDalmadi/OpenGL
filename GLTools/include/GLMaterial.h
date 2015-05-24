125c1e9b7725011fb4e0bc69725da4638076c23a
Zoltan Dalmadi <dmz985@gmail.com>
2015-05-24 13:04:05 +0200

    Cleanup

--- GLTools/include/GLMaterial.h
+++ GLTools/include/GLMaterial.h
@@ -10,13 +10,13 @@ class GLMaterial
 {
 public:
   glm::vec3 m_diffuse;
-  std::unique_ptr<GLTexture> m_diffuseTex;
-  int m_diffuseTex_index;
-  float m_diffMix;
   glm::vec3 m_specular;
-  std::unique_ptr<GLTexture> m_specularTex;
+  std::shared_ptr<GLTexture> m_diffuseTex;
+  std::shared_ptr<GLTexture> m_specularTex;
+  int m_diffuseTex_index;
   int m_specularTex_index;
   float m_specMix;
+  float m_diffMix;
   float m_shininess;
 
   GLMaterial();
