507f304d8020bc57c0bb109f8e3ee4356430aa30
Zoltan Dalmadi <dmz985@gmail.com>
2015-06-27 19:34:13 +0200

    Added Enemy class

---
+++ TowerDefense/include/Enemy.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <utility>
+#include "GLModel.h"
+
+class Enemy
+{
+public:
+  explicit Enemy(GLTools::GLModel *model,
+                 const glm::vec3& pos = glm::vec3(0.0f),
+                 const glm::vec3& dir = glm::vec3(0.0f, 0.0f, 1.0f));
+
+  virtual ~Enemy();
+
+  const glm::vec3& getPosition() const;
+  void setPosition(const glm::vec3& pos);
+
+  const glm::vec3& getDirection() const;
+  void setDirection(const glm::vec3& dir);
+
+  void damage(float damage);
+  bool isDestroyed() const;
+
+  void draw(const GLTools::GLShaderProgram& shaderProgram);
+
+  std::pair<glm::vec3, glm::vec3> calculate_AABB();
+
+private:
+  void update();
+  GLTools::GLModel *m_model;
+  glm::vec3 m_position;
+  glm::vec3 m_direction;
+
+  glm::mat4 m_modelMatrix;
+
+  float m_progress = 0.0f;
+  float m_velocity = 0.001f;
+  float m_hitPoints = 100.0f;
+  bool m_destroyed = false;
+};
