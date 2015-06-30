cfbd75b05e8422c23b55c452fb68c7ab9b84ef58
Zoltan Dalmadi <dmz985@gmail.com>
2015-06-30 12:32:06 +0200

    Optimizations

--- GLTools/src/GLCurvePath.cpp
+++ GLTools/src/GLCurvePath.cpp
@@ -1,28 +1,24 @@
 #include "GLCurvePath.h"
-#include <iostream>
 
 GLTools::GLCurvePath::GLCurvePath()
-{
-}
-
+{}
 
 GLTools::GLCurvePath::~GLCurvePath()
-{
-}
+{}
 
-std::pair<glm::vec3, glm::vec3> GLTools::GLCurvePath::getPositionAndTangent(
-  float t)
+std::pair<glm::vec3, glm::vec3> GLTools::GLCurvePath::getPositionAndTangent
+(float t)
 {
   std::pair<glm::vec3, glm::vec3> ret;
 
   auto size = m_data.size();
   auto oneSlashSize = 1.0f / size;
 
-  for (int i = 1; i <= size; i++)
+  for (auto i = 1; i <= size; i++)
   {
     if (t <= i * oneSlashSize)
     {
-      auto vt = (t - ((i - 1) * oneSlashSize)) * size;
+      auto vt = (t - (i - 1) * oneSlashSize) * size;
 
       ret = m_data[i - 1].getPositionAndTangent(vt);
       break;
@@ -34,8 +30,6 @@ std::pair<glm::vec3, glm::vec3> GLTools::GLCurvePath::getPositionAndTangent(
 
 void GLTools::GLCurvePath::draw()
 {
-  for (auto i : m_data)
-  {
-    i.draw();
-  }
+  for (auto& curve : m_data)
+    curve.draw();
 }
