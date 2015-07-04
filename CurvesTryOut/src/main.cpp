4ee6f29cf711667be470afcbd8471d6f5065caba
Orban Istvan <firstvan@live.com>
2015-07-04 13:02:03 +0200

    Text drawout.

--- CurvesTryOut/src/main.cpp
+++ CurvesTryOut/src/main.cpp
@@ -13,8 +13,22 @@
 #include "GLPointLight.h"
 #include "GLCurves.h"
 #include "GLCurvePath.h"
+#include <map>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 
 //A görbe kipróbálása, illettve tárgyak görbe mentén lévõ mozgatásának kipróbálására készült project
+
+struct Character
+{
+  GLuint     TextureID;  // ID handle of the glyph texture
+  glm::ivec2 Size;       // Size of glyph
+  glm::ivec2 Bearing;    // Offset from baseline to left/top of glyph
+  GLuint     Advance;    // Offset to advance to next glyph
+};
+
+std::map<GLchar, Character> Characters;
 
 using namespace GLTools;
 
@@ -123,6 +137,9 @@ void init()
 
   glViewport(0, 0, WIDTH, HEIGHT);
   glEnable(GL_DEPTH_TEST);
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   //glEnable(GL_CULL_FACE);
   glLineWidth(5);
   glPointSize(10);
@@ -183,11 +200,163 @@ void setupShaders1(GLShaderProgram& shaderProgram)
   std::cout << shaderProgram.log() << std::endl;
 }
 
+void setupShaders2(GLShaderProgram& shaderProgram)
+{
+  auto vertexShader = std::make_shared<GLShader>
+                      (GLShader::shaderType::VERTEX_SHADER);
+  vertexShader->loadSource("shaders/text_vertex_shader.glsl");
+  vertexShader->compile();
+  std::cout << vertexShader->log() << std::endl;
+
+  auto fragmentShader = std::make_shared<GLShader>
+                        (GLShader::shaderType::FRAGMENT_SHADER);
+  fragmentShader->loadSource("shaders/text_fragment_shader.glsl");
+  fragmentShader->compile();
+  std::cout << fragmentShader->log() << std::endl;
+
+  shaderProgram.create();
+  shaderProgram.addShader(vertexShader);
+  shaderProgram.addShader(fragmentShader);
+  shaderProgram.link();
+  std::cout << shaderProgram.log() << std::endl;
+}
+
+GLuint VAO, VBO;
+void RenderText(GLShaderProgram& s, std::string text, GLfloat x,
+                GLfloat y,
+                GLfloat scale, glm::vec3 color)
+{
+  // Activate corresponding render state
+  s.use();
+  s.setUniformValue("textColor", color.x, color.y,
+                    color.z);
+  glActiveTexture(GL_TEXTURE0);
+  glBindVertexArray(VAO);
+
+  // Iterate through all characters
+  std::string::const_iterator c;
+
+  for (c = text.begin(); c != text.end(); c++)
+  {
+    Character ch = Characters[*c];
+
+    GLfloat xpos = x + ch.Bearing.x * scale;
+    GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+    GLfloat w = ch.Size.x * scale;
+    GLfloat h = ch.Size.y * scale;
+    // Update VBO for each character
+    GLfloat vertices[6][4] =
+    {
+      { xpos, ypos + h, 0.0, 0.0 },
+      { xpos, ypos, 0.0, 1.0 },
+      { xpos + w, ypos, 1.0, 1.0 },
+
+      { xpos, ypos + h, 0.0, 0.0 },
+      { xpos + w, ypos, 1.0, 1.0 },
+      { xpos + w, ypos + h, 1.0, 0.0 }
+    };
+    // Render glyph texture over quad
+    glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+    // Update content of VBO memory
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // Render quad
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+    x += (ch.Advance >> 6) *
+         scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
+  }
+
+  glBindVertexArray(0);
+  glBindTexture(GL_TEXTURE_2D, 0);
+}
 
 int main()
 {
   init();
 
+  // szöveg rendereléshez kell
+
+  FT_Library ft;
+
+  if (FT_Init_FreeType(&ft))
+    std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+  FT_Face face;
+
+  if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+    std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+
+  FT_Set_Pixel_Sizes(face, 0, 48);
+
+  if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+    std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
+
+  for (GLubyte c = 0; c < 128; c++)
+  {
+    // Load character glyph
+    if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+    {
+      std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+      continue;
+    }
+
+    // Generate texture
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(
+      GL_TEXTURE_2D,
+      0,
+      GL_RED,
+      face->glyph->bitmap.width,
+      face->glyph->bitmap.rows,
+      0,
+      GL_RED,
+      GL_UNSIGNED_BYTE,
+      face->glyph->bitmap.buffer
+    );
+    // Set texture options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Now store character for later use
+    Character character =
+    {
+      texture,
+      glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+      glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+      face->glyph->advance.x
+    };
+    Characters.insert(std::pair<GLchar, Character>(c, character));
+  }
+
+  glBindTexture(GL_TEXTURE_2D, 0);
+  // Destroy FreeType once we're finished
+  FT_Done_Face(face);
+  FT_Done_FreeType(ft);
+
+  auto shaderProgram2 = std::make_unique<GLShaderProgram>();
+  setupShaders2(*shaderProgram2);
+
+  glm::mat4 projection2 = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  //eddig szöveghez kellõ cuccok
   auto defaultMaterial = std::make_unique<GLTools::GLMaterial>();
 
   floorPlane = std::make_unique<GLPlane>(100.0f, 100.0f);
@@ -205,12 +374,6 @@ int main()
 
   float t = 0.0f;
 
-
-
-  std::pair<glm::vec3, glm::vec3> asd1 = path.getPositionAndTangent(1.0f);
-  std::cout << t << ": " << asd1.first.x << " " << asd1.first.y << " " <<
-            asd1.first.z << " :: " << asd1.second.x << " " << asd1.second.y << " " <<
-            asd1.second.z << "\n";
 
   while (!glfwWindowShouldClose(window))
   {
@@ -232,6 +395,15 @@ int main()
 
 
     path.draw();
+    shaderProgram2->use();
+    shaderProgram2->setUniformValue("projection",
+                                    projection);
+    RenderText(*shaderProgram2, "This is sample text", 25.0f, 25.0f, 1.0f,
+               glm::vec3(0.5,
+                         0.8f, 0.2f));
+    RenderText(*shaderProgram2, "(C) LearnOpenGL.com", 540.0f, 570.0f, 0.5f,
+               glm::vec3(0.3,
+                         0.7f, 0.9f));
     //std::pair<glm::vec3, glm::vec3> asd = path.getPositionAndTangent(t);
     /*std::cout << t << ": " << asd.first.x << " " << asd.first.y << " " <<
               asd.first.z << " :: " << asd.second.x << " " << asd.second.y << " " <<
